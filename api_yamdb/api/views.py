e9fcd1f997791bc4b5980c7ebb3b3a3d244c8734
Дмитрий Шапченко <dmitryshapchenko@yandex.ru>
2023-04-07 22:10:51 +0300

    project test 1

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -82,7 +82,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
     def get_title(self):
         title = get_object_or_404(Title, id=self.kwargs.get("title_id"))
-        return
+        return title
 
     def perform_create(self, serializer):
         title = self.get_title()
@@ -110,7 +110,7 @@ class CommentViewSet(viewsets.ModelViewSet):
             Review,
             id=self.kwargs.get("review_id"),
         )
-        return
+        return review
 
     def perform_create(self, serializer):
         review = self.get_review()
